d085e1f8d6eba01fe4e51a949d0503a77600fe59
Martin <martyofmca@gmail.com>
2023-08-12 06:44:31 -0700

    add command-line interpreter

---
+++ console.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Defines a module that created the command-line
+interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Define the command-line interpreter """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """ A handler to quit the interpreter """
+        return (True)
+
+    def help_quit(self):
+        """
+        The handler for providing help for the quit command
+        """
+        self.exit_helper_text()
+
+    def do_EOF(self, arg):
+        """ A handler to Ctrl + D signal """
+        return (True)
+
+    def help_EOF(self):
+        """
+        The handler for providing help for the EOF command
+        """
+        self.exit_helper_text()
+
+    def emptyline(self):
+        """ A handler for emptylines """
+        pass
+
+    def exit_helper_text(self):
+        """
+        Prints the instructions for the quit and
+        EOF commands when the help utility
+        is invoked
+        """
+        print("Quit the command-line interface\n")
+
+
+if (__name__ == "__main__"):
+    HBNBCommand().cmdloop()
